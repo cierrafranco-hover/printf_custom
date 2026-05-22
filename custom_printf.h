f2b7bdb4493fc8ec6979048b1ab1edbb4929596d
cierra Franco-Hover <cierra.franco-hover@atlasstudents.com>
2026-05-22 18:40:24 +0000

    updated-code-I-previously-pushed,-char,-string,-num,-specifier and .h to match .c file

--- custom_printf.h
+++ custom_printf.h
@@ -1,6 +1,5 @@
-#ifndef CUSTOM_PRINT_H
-#define CUSTOM_PRINT_H
-
-int customPrintf(char *format, ...);
-
+#ifndef CUSTOM_PRINTF_H
+#define CUSTOM_PRINTF_H
+#include <stdarg.h>
+int customPrintf(const char *formatString, ...);
 #endif
