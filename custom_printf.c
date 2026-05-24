bf2b1cc3bb520ec81d38f1b22912f8015ccb5ebe
JTfromIT918 <justin.roudebush@atlasstudents.com>
2026-05-24 09:32:10 -0500

    Finished customprint and binary support

--- custom_printf.c
+++ custom_printf.c
@@ -1,111 +1,4 @@
-#include "custom_printf.h"
-<<<<<<< HEAD
-#include <stdarg.h>
-#include <unistd.h>
-
-/* -----------------------------
-   Helper: print a single char
-------------------------------*/
-static int printChar(char c)
-{
-    write(1, &c, 1);
-    return 1;
-}
-
-/* -----------------------------
-   Helper: print a string
-------------------------------*/
-static int printString(char *str)
-{
-    int count = 0;
-
-    if (!str)
-        return write(1, "(null)", 6);
-
-    while (*str)
-        count += write(1, str++, 1);
-
-    return count;
-}
-
-/* -----------------------------
-   Helper: print binary (%b)
-------------------------------*/
-static int printBinary(unsigned int n)
-{
-    int count = 0;
-    int bits[32];
-    int i = 0;
-
-    if (n == 0)
-        return write(1, "0", 1);
-
-    while (n > 0)
-    {
-        bits[i++] = n % 2;
-        n /= 2;
-    }
-
-    while (i--)
-    {
-        char c = bits[i] + '0';
-        count += write(1, &c, 1);
-    }
-
-    return count;
-}
-
-/* -----------------------------
-   MAIN FUNCTION
-------------------------------*/
-int customPrintf(char *format, ...)
-{
-    va_list args;
-    int i = 0;
-    int count = 0;
-
-    if (!format)
-        return write(1, "Error: formatString is NULL", 28);
-
-    va_start(args, format);
-
-    while (format[i])
-    {
-        if (format[i] != '%')
-        {
-            count += write(1, &format[i], 1);
-        }
-        else
-        {
-            i++;
-
-            /* FIX: prevent reading past end of string */
-            if (format[i] == '\0')
-                break;
-
-            if (format[i] == 'c')
-                count += printChar(va_arg(args, int));
-
-            else if (format[i] == 's')
-                count += printString(va_arg(args, char *));
-
-            else if (format[i] == 'b')
-                count += printBinary(va_arg(args, unsigned int));
-
-            else if (format[i] == '%')
-                count += write(1, "%", 1);
-
-            /* TODO: add d / i handling */
-        }
-
-        i++;
-    }
-
-    va_end(args);
-    return count;
-}
-=======
-#include <unistd.h>
+#include "custom_printf.h"#include <unistd.h>
 #include <stdarg.h>
 
 static int printChar(char c)
@@ -156,7 +49,18 @@ static int printNumber(int num)
 }
 
 static int printBinary(unsigned int num)
-{}
+{ int count = 0;
+
+    //If number is greater than 1, keep dividing recursively 
+    if (num > 1)
+    {
+        count += printBinary(num / 2);
+    }
+
+    // Print current binary digit 
+    count += printChar((num % 2) + '0');
+
+    return count;}
 
 static int handleSpecifier(char specifier, va_list *args)
 {
@@ -174,5 +78,30 @@ static int handleSpecifier(char specifier, va_list *args)
 }
 
 int customPrintf(const char *formatString, ...)
-{}
->>>>>>> f2b7bdb4493fc8ec6979048b1ab1edbb4929596d
+{ va_list args;
+    int i = 0;
+    int count = 0;
+
+    va_start(args, formatString);
+
+    while (formatString[i] != '\0')
+    {
+        if (formatString[i] == '%')
+        {
+            i++;
+
+            /* Handle format specifier */
+            count += handleSpecifier(formatString[i], &args);
+        }
+        else
+        {
+            /* Print regular character */
+            count += printChar(formatString[i]);
+        }
+
+        i++;
+    }
+
+    va_end(args);
+
+    return count;}
